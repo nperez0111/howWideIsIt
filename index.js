9fc86e8e3db15b12c8c22a03b73c8e641e83b36e
Nick The Sick <nperez0111@gmail.com>
2016-07-15 13:21:50 -0400

    added computation of diagnol needed

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ function computeHeight( aspWidth, aspHeight, lengthDiag ) {
     return computeUnit( aspWidth, aspHeight, lengthDiag ) * aspHeight;
 }
 
+function computeDiag( aspW, aspH, length, bool ) {
+    var val = length,
+        multiplier = aspW / aspH;
+    if ( bool ) {
+        multiplier = 1 / multiplier;
+    }
+    return Math.sqrt( Math.pow( length, 2 ) + Math.pow( val * multiplier, 2 ) );
+}
+
 module.exports = function ( aspWidth, aspHeight, lengthDiag ) {
     var aspWidth = aspWidth,
         aspHeight = aspHeight,
@@ -25,6 +34,26 @@ module.exports = function ( aspWidth, aspHeight, lengthDiag ) {
     return {
         height: computeHeight( aspWidth, aspHeight, lengthDiag ),
         width: computeWidth( aspWidth, aspHeight, lengthDiag ),
+        aspRatio: aspWidth + ':' + aspHeight,
+        diaganol: lengthDiag
+    }
+}
+
+module.exports.diag = function ( aspWidth, aspHeight, length, isWidth ) {
+    var aspWidth = aspWidth,
+        aspHeight = aspHeight,
+        isWidth = isWidth === undefined ? true : isWidth,
+        lengthDiag = computeDiag( aspWidth, aspHeight || 0, length || 0, isWidth );
+    if ( aspHeight === undefined && length === undefined && isWidth === undefined ) {
+        aspHeight = aspWidth.height;
+        length = aspWidth.length;
+        isWidth = aspWidth.lengthIsOnWideSide;
+        aspWidth = aspWidth.width;
+        lengthDiag = computeDiag( aspWidth, aspHeight || 0, length || 0, isWidth );
+    }
+    return {
+        height: isWidth ? aspHeight * length / aspWidth : length,
+        width: isWidth ? length : aspWidth * length / aspHeight,
         aspRatio: aspWidth + ':' + aspHeight,
         diaganol: lengthDiag
     }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,10 @@
 var obj = require( './../index.js' );
+
 console.log( obj( 16, 9, 27 ) )
 console.log( obj( {
     width: 16,
     height: 9,
     diag: 27
 } ) )
+console.log( obj.diag( 16, 9, 23 ) )
+console.log( obj.diag( 16, 9, 23, false ) )
