c0ef0265c74ed284231ebb68816f9393d8f58e45
Nick The Sick <nperez0111@gmail.com>
2016-07-15 21:33:53 -0400

    added diag option to CLI

--- bin/index.js
+++ bin/index.js
@@ -7,26 +7,30 @@ var getUsage = require( 'command-line-usage' ),
         summary: 'Use this mode if you have the aspect ratio and the diagonal. Does not need to be specified.'
     }, {
         name: 'diagonal',
-        summary: "Use this mode if you have the aspect ratio and one side. The side defaults to the width but can be reversed by adding the -r flag. Usage: how-wide diagonal -w 16 -h 9 -l 23 "
+        summary: "Use this mode if you have the aspect ratio and one side. The side defaults to the width but can be reversed by adding the -r flag. Usage: how-wide --diag -w 16 -h 9 -l 23 "
     } ],
     optionDefinitions = [ {
         name: 'help',
         description: 'Display this usage guide.'
-    }, {
-        name: 'height',
-        alias: 'h',
-        typeLabel: '[underline]{number}',
-        description: "The [underline]{height} of the aspect ratio. Ex: 16:9 it's the 9, 4:3 it's the 3"
     }, {
         name: 'width',
         alias: 'w',
         typeLabel: '[underline]{number}',
         description: "The [underline]{width} of the aspect ratio. Ex: 16:9 it's the 16, 4:3 it's the 4"
     }, {
+        name: 'height',
+        alias: 'h',
+        typeLabel: '[underline]{number}',
+        description: "The [underline]{height} of the aspect ratio. Ex: 16:9 it's the 9, 4:3 it's the 3"
+    }, {
         name: 'length',
         alias: 'l',
         typeLabel: '[underline]{number}',
         description: "The length of the diagonal inches across. OR if in diagonal mode the length of the width side."
+    }, {
+        name: 'diag',
+        alias: 'd',
+        description: 'Sets the mode to be in diagonal mode meaning we are searching for the diagonal of the screen.'
     }, {
         name: 'reverse',
         alias: 'r',
@@ -38,6 +42,7 @@ var getUsage = require( 'command-line-usage' ),
     } ) ).length,
     final = require( './../index.js' );
 
+
 function prettyPrint( obj ) {
     console.log( getUsage( [ {
         header: 'Results for ' + obj.aspRatio + ' ' + obj.diagonal + "' TV",
@@ -48,35 +53,53 @@ function prettyPrint( obj ) {
             desc: 'Height is:',
             example: obj.height + "'"
         } ]
-    } ] ) )
+    } ] ) );
 }
 
+function warnPrior( obj ) {
+    console.log( getUsage( [ {
+        header: 'Diagonal Length is: ' + obj.diagonal,
+        content: [ {
+            desc: '',
+            example: ''
+        } ]
+    } ] ) );
+    return obj;
+}
 
 function printHelp() {
     console.log( getUsage( [ {
         header: 'How Wide Is It Cli Tool',
         content: 'This Program helps determine the actual dimensions of a tv when given the aspect ratio and diagonal inches.'
     }, {
-        header: 'Command List',
+        header: 'Possible Modes',
         content: contently
     }, {
         header: 'Options',
         optionList: optionDefinitions
     } ] ) );
 }
-
 if ( process.argv.slice( 2 ).length == 3 ) {
 
     prettyPrint( final.apply( null, process.argv.slice( 2 ).map( function ( a ) {
         return parseFloat( a );
     } ) ) );
 
+} else if ( process.argv.slice( 2 ).length == 4 && process.argv.slice( 2 ).indexOf( '-d' ) > -1 ) {
+    var val = process.argv.slice( 2 );
+    val.splice( val.indexOf( '-d' ), 1 );
+    prettyPrint( warnPrior( final.diag.apply( null, val.map( function ( a ) {
+        return parseFloat( a );
+    } ) ) ) );
+
 } else if ( argsLength !== 3 || parsedArgs.help === true || ( Object.keys( parsedArgs ).length == 1 && parsedArgs.height === true ) ) {
 
     printHelp();
 
 } else {
-
-    prettyPrint( final( parseFloat( parsedArgs.width ), parseFloat( parsedArgs.height ), parseFloat( parsedArgs.length ) ) )
-
+    if ( parsedArgs.diag == true ) {
+        prettyPrint( warnPrior( final.diag( parseFloat( parsedArgs.width ), parseFloat( parsedArgs.height ), parseFloat( parsedArgs.length ), parsedArgs.reverse === undefined ? true : parsedArgs.reverse ) ) );
+    } else {
+        prettyPrint( final( parseFloat( parsedArgs.width ), parseFloat( parsedArgs.height ), parseFloat( parsedArgs.length ) ) )
+    }
 }
