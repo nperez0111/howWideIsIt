33da43175a65916f9325677ed2ba59108071873d
Nick The Sick <nperez0111@gmail.com>
2016-07-16 21:02:02 -0400

    let's get on this

---
+++ app/scripts/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const Ract = require( 'ractive/ractive.runtime' ),
+    howWide = require( 'how-wide-is-it' );
---
+++ test/app_test.js
@@ -0,0 +1,12 @@
+/* jshint expr: true */
+'use strict';
+
+require('../app/scripts/app');
+
+describe('how-wide-is-it', function () {
+  describe('give it some context', function () {
+    it('should assert something', function () {
+      expect(true).to.be.true;
+    });
+  });
+});
